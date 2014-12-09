40fcb332d3a028c61affa7985ef6155d7e05a8e2
Vaibhav Desai <desai043@umn.edu>
2014-12-09 01:14:37 -0600

    Updates for 1D binomial filter convolution

--- CudaUtilities.h
+++ CudaUtilities.h
@@ -6,6 +6,9 @@
 // This file contains helper functions for kernel/wrapper implementations
 //
 //
+// Created: 3-Dec-2014
+//
+//
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 #ifndef CUDAUTILITIES_H
@@ -19,6 +22,18 @@
 #include <helper_cuda.h>
 #include <helper_functions.h>
 #include <helper_timer.h>
+#include <driver_types.h>
+
+
+#define GPUERRORCHECK(call) { ReportGPUError((call), __FILE__, __LINE__);}
+inline void ReportGPUError(cudaError_t ret_code, const char *file, int line)
+{
+    if (ret_code != cudaSuccess)
+    {
+        fprintf(stderr, "ReportGPUError: %s %s %d\n", cudaGetErrorString(ret_code), file, line);
+        exit(1);
+    }
+}
 
 
 // Timer utility
@@ -45,7 +60,7 @@ public:
     float stopAndGetTimerValue();
 };
 
-
+// CUDA Memory handling functions
 void
 CopyGPUMemory(void* dest, void* src, unsigned int num_elems, bool HtoD);
 
@@ -58,6 +73,7 @@ FreeGPUMemory(void* ptr);
 void
 CopyToGPUConstantMemory(void* dest, void* src, int numBytes);
 
+// Computed data verification
 bool
 VerifyComputedData(float* reference, float* data, int numElems);
 
